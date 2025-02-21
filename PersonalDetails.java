e25f9d84f1f4f15fb835f2d02a1ac4595e842f61
SubekshyaTimalsina <Tsubekshya23@tbc.edu.np>
2025-02-21 22:35:58 +0545

    Final Commit

---
+++ PersonalDetails.java
@@ -0,0 +1,10 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        System.out.println("Personal Details:");
+        System.out.println("Name: Subekshya Timalsina");
+        System.out.println("Address: Kathmandu, Nepal");
+        System.out.println("Gender: Female");
+        System.out.println("Phone Number: +977 9702628568");
+        System.out.println("Email: subekshyatimalsina@gmail.com");
+    }
+}
